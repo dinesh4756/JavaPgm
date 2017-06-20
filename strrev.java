a73cf36e6411aa5bb45cd0d177b45edb50f44a63
dinesh4756 <dineshcse@outlook.com>
2017-06-20 14:38:59 +0530

    Create strrev.java

---
+++ strrev.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class strrev
+{
+public static void main(String args[])
+{
+String a,b;
+Scanner in=new Scanner(System.in);
+System.out.println("Enter the string");
+a=in.nextLine();
+b=a.reverse();
+System.out.println("Reversed value=" + b);
+}
+}
