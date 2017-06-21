292d648c21b69e5fe066547ee9e309ff622a5e8b
dinesh4756 <dineshcse@outlook.com>
2017-06-21 11:57:49 +0530

    Create evenodd.java

---
+++ evenodd.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class evenodd
+{
+public static void main(String args[])
+{
+int n;
+Scanner in=new Scanner(System.in);
+System.out.println("Enter the number");
+n=in.nextInt();
+if(n%2==0)
+System.out.println("Even");
+else
+System.out.println("Odd");
+}
+}
