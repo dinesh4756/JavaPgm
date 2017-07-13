cb2685cad5f01cdc68d7127f955e4ded2f8e39ee
dinesh4756 <dineshcse@outlook.com>
2017-07-13 16:42:23 +0530

    Create bigg.java

---
+++ bigg.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class bigg
+{
+public static void main(String args[])
+{
+int a=5,b=10,c=4;
+if(a>b && a>C)
+System.out.println("A is big");
+else if(b>a && b>c)
+System.out.println("B is big");
+else
+System.out.println("C is big");
+}
+}
