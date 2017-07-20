bbba35027b21b9aba0b6d7080b208718a8eea2e9
dinesh4756 <dineshcse@outlook.com>
2017-07-20 18:09:34 +0530

    Create primerange.java

---
+++ primerange.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class primerange
+{
+    public static void main(String args[])
+    {
+         int s1, s2, s3, flag = 0, i, j;
+         Scanner s = new Scanner(System.in);
+         System.out.println ("Enter the limit :"); 
+         s1 = s.nextInt();
+         System.out.println ("Enter the limit :"); 
+         s2 = s.nextInt();
+         System.out.println ("The prime numbers are :");
+         for(i = s1; i <= s2; i++)
+         {
+             for( j = 2; j < i; j++)
+             {
+                 if(i % j == 0)
+                 {
+                     flag = 0;
+                     break;
+                 }
+                 else
+                 {
+                     flag = 1;
+                 }
+             }
+             if(flag == 1)
+             {
+                 System.out.println(i);
+             }
+         }
+    }
+}
